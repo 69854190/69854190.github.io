89cd862ce3a66d4d27882c9e344347ffb14c555b
69854190 <69854190@qq.com>
2023-04-07 22:00:26 +0800

    Site updated: 2023-04-07 22:00:24

--- js/matery.js
+++ js/matery.js
@@ -108,8 +108,8 @@ $(function () {
 	/*
     $(document).find('img[data-original]').each(function(){
      $(this).parent().attr("href", $(this).attr("data-original"));
-      });*/
-
+      });
+*/
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
         if (progressElement) {
